ca0b05838653e16bd65d028095f969be4eb01bf4
lxX-lorias-Xxl <oriasdiego34@gmail.com>
2020-10-07 13:29:59 -0600

    Cambios recientes

---
+++ Entidades/Ejemplo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ejemplo { 
+   
+
+        [DataMember]
+
+        public string CategoriaID { get; set; }
+
+        [DataMember]
+
+        public string Departamento { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [MaxLength(50), MinLength(3)]
+
+        [DataMember]
+
+
+
+        public string Direccion { get; set; }
+
+        [DataMember]
+
+        public string Telefono { get; set; }
+
+        [DataMember]
+
+        public string Correo { get; set; }
+
+        [DataMember]
+
+
+        public string Clave { get; set; }
+
+        [DataMember]
+
+        public string Codigo { get; set; }
+
+        [DataMember]
+
+        public List<RRHH> recurso { get; set; }
+    }
+}
+
